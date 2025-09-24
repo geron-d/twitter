256c1db2adc2fa422e857a7c5a1b1d65d2d58b9e
Maxim Zhevnov <MZhevnov@alfabank.ru>
2025-09-24 18:11:23 +0300

    refactor: move enums to common-lib

--- services/users-api/src/main/java/com/twitter/enums/UserRole.java
+++
@@ -1,8 +0,0 @@
-package com.twitter.enums;
-
-public enum UserRole {
-    USER,
-    ADMIN,
-    MODERATOR
-}
-
--- services/users-api/src/main/java/com/twitter/enums/UserStatus.java
+++
@@ -1,9 +0,0 @@
-package com.twitter.enums;
-
-/**
- * Enum representing the status of a user account
- */
-public enum UserStatus {
-    ACTIVE,
-    INACTIVE
-}
