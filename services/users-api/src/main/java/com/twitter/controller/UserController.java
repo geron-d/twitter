770e8758df1005e03e6006acd9e25ed4c6126592
geron-d <83971294+geron-d@users.noreply.github.com>
2025-10-05 22:27:34 +0300

    Feature/users api/find all pagong default (#7)
    * feat: add default pagination configuration
    
    * feat: add @PageableDefault annotation to controller
    
    * test: update pagination tests and documentation
    
    * feat: finish paginating default

--- services/users-api/src/main/java/com/twitter/controller/UserController.java
+++ services/users-api/src/main/java/com/twitter/controller/UserController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -70,7 +71,8 @@ public class UserController {
      */
     @LoggableRequest
     @GetMapping
-    public PagedModel<UserResponseDto> findAll(@ModelAttribute UserFilter userFilter, Pageable pageable) {
+    public PagedModel<UserResponseDto> findAll(@ModelAttribute UserFilter userFilter, 
+                                               @PageableDefault(size = 10) Pageable pageable) {
         Page<UserResponseDto> users = userService.findAll(userFilter, pageable);
         return new PagedModel<>(users);
     }
